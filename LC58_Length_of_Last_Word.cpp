98df4df1ff25d138c09aedd09f7ecb7bf510664e
SHUBHO CHAKRABORTY <shubho1chakraborty1@gmail.com>
2021-10-12 14:45:33 +0530

    Day 24

---
+++ LC58_Length_of_Last_Word.cpp
@@ -0,0 +1,15 @@
+//Time:  O(N);
+//Space: O(1);
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans = 0;
+        for(int i=s.length()-1;i >= 0;i--){
+            if(s[i] == ' ' && ans != 0)
+                return ans;
+            if(s[i] != ' ')
+                ans++;
+        }
+        return ans;
+    }
+};
