785e1b32542a269413e36f90824ef719453ae1b7
SHUBHO CHAKRABORTY <shubho1chakraborty1@gmail.com>
2021-11-11 22:02:39 +0530

    Day 46

---
+++ LC217_Contains_Duplicate.cpp
@@ -0,0 +1,14 @@
+//Time:  O(N);
+//Space: O(N);
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,bool>map;
+        for(int i=0;i<nums.size();i++){
+            if(map.find(nums[i]) != map.end())
+                return true;
+            map[nums[i]] = true;
+        }
+        return false;
+    }
+};
