3d8192bfbc4d3e9c440c02f7caffd8036bfefc60
SHUBHO CHAKRABORTY <shubho1chakraborty1@gmail.com>
2021-11-18 23:52:03 +0530

    Day 51

---
+++ LC210_Course_Schedule_II.cpp
@@ -0,0 +1,30 @@
+//Time:  O(N+E);
+//Space: O(N+E) + O(N) + O(N);
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> findOrder(int numCourses, vector<vector<int>>& pr) {
+        vector<vector<int>> v(numCourses);
+        for(int i=0;i<pr.size();i++)
+            v[pr[i][0]].push_back(pr[i][1]);
+        vector<int> visited(numCourses, 0);
+        for(int i=0;i<numCourses;i++){
+            if(visited[i] == 0)
+                if(isCyclic(i,v,visited))
+                    return {};
+        }
+        return ans;
+    }
+    bool isCyclic(int curr, vector<vector<int>>& v, vector<int>& visited){
+        if(visited[curr] == 2) return true;
+        visited[curr] = 2;
+        for(int i=0;i<v[curr].size();i++){
+            if(visited[v[curr][i]] != 1)
+                if(isCyclic(v[curr][i],v,visited))
+                    return true;
+        }
+        visited[curr] = 1;
+        ans.push_back(curr);
+        return false;
+    }
+};
