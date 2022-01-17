8586669fae5b39cbf84e6991f4111563a2e1d81c
SHUBHO CHAKRABORTY <shubho1chakraborty1@gmail.com>
2022-01-17 22:54:41 +0530

    Day 74

---
+++ LC287_Find_the_Duplicate_Numbers.cpp
@@ -0,0 +1,18 @@
+//Time:  O(N);
+//Space: O(1);
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0], fast= nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(fast!=slow);
+        fast = nums[0];
+        while(fast!=slow){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return fast;
+    }
+};
